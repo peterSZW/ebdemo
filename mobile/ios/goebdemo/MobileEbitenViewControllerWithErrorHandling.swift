404a446423f4905852c0dd1abd4024e9fdb012ca
peter <peta@21cn.com>
2021-10-17 16:31:51 +0800

    add ios project

---
+++ mobile/ios/goebdemo/MobileEbitenViewControllerWithErrorHandling.swift
@@ -0,0 +1,22 @@
+//
+//  MobileEbitenViewControllerWithErrorHandling.swift
+//  godanmaku
+//
+//  Created by Yota Hamada on 2020/08/20.
+//  Copyright © 2020 Yota Hamada. All rights reserved.
+//
+
+import Foundation   
+import Mobile
+
+class MobileEbitenViewControllerWithErrorHandling: MobileEbitenViewController {
+    override func onError(onGameUpdate err: Error!) {
+        print(err ?? "OnGameUpdate Error")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let bounds = UIScreen.main.bounds
+        //MobileSetWindowSize(Int(bounds.width), Int(bounds.height))
+    }
+}
